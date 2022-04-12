cf128af5e8974eb3d4c478fb23a708fa97d10fef
Muthu <muthu@Muthus-MacBook-Pro.local>
2022-04-12 15:42:13 +0530

    #task done about directives...

--- angular-ZerotoHero/src/app/app.component.ts
+++ angular-ZerotoHero/src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-ZerotoHero';
+  inputValue?: string;
+  btnDisabled: boolean = false;
+  buttonDisabled: boolean = true;
+  numbers: number[] = Array(50);
+  selctedNumber: number = 0;
+
+  onSubmit() {
+    if(this.inputValue == "submitted") {
+      this.btnDisabled = true;
+    }
+    else {
+      this.btnDisabled = false;
+    }
+  }
+
+  clickedNumber(value:number) {
+    this.selctedNumber = value;
+    this.buttonDisabled = false;
+  }
+
+  clear() {
+    this.buttonDisabled = true;
+    this.selctedNumber = 0;
+  }
 }
